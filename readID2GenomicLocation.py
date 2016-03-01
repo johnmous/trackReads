fde5f7cdd443ad8bce6e96bbf6ebbd707c1b2382
Ioannis Moustakas <johnmous@gmail.com>
2016-03-01 11:45:13 +0100

    First commit of read tracking scripts. They are intended to track reads using the read ID across different alignment files. Used as means of comparisson between the output of RNA seq protocols

---
+++ readID2GenomicLocation.py
@@ -0,0 +1,114 @@
+#!/zfs/datastore0/software/v_envs/py333/bin/python3.3
+# Author: Ioannis Moustakas
+# Title: Input a number of Read IDs and return their position on the genome. Use 
+
+import pysam
+import sys
+ 
+# Save bam File from argv 
+bamFile =str(sys.argv[1])
+annotationFile =str(sys.argv[2])
+
+# Store readIDs from STDIN in an array
+readIDs=[]
+for line in sys.stdin:
+    # chomp the last character - new line    
+    line = line[:-1]       
+    # If line not empty
+    if line: 
+        readIDs.append(line)
+
+
+
+""" A function to read the simplified annotation file and return a dictionary 
+    chromosome => [start, stop, geneID] for all gene IDs
+""" 
+def getAnnotation(annotationFile):
+    fileHandle=open(annotationFile, 'r')
+    annotation={}
+    for line in fileHandle:
+        line=line[:-1] 
+        listOfAtr=line.split('\t')
+        chrom=listOfAtr[0]
+        startPos=listOfAtr[1]
+        stopPos=listOfAtr[2]
+        geneID=listOfAtr[3]
+        
+        # build a dictionary chromosome => [start, stop, geneID]
+        if chrom not in annotation:
+            annotation[chrom]=[[startPos, stopPos, geneID]]   
+        else:
+            annotation[chrom].append([startPos, stopPos, geneID])     
+    return(annotation)
+
+
+# take chromosome and postition and anotation dictionary and return geneID(s)        
+def annotateLocation(chrom, pos, annotation):
+    geneIDsList=[]
+    chromosomeAnnot=annotation[chrom]
+    for i in range(len(chromosomeAnnot)):
+        annotElement = chromosomeAnnot[i]
+        if int(annotElement[0])<pos and int(annotElement[1])>pos:
+            geneIDsList.append(annotElement[2])
+    if len(geneIDsList)==0:
+        geneIDsList=["NonAnnotatedRegion"]
+    return(geneIDsList)
+
+
+def getReadLocation(bamFile, annotation):
+    samFile=pysam.AlignmentFile(bamFile, 'rb')
+    # readID to chromosome, position
+    readIDToAttr={}
+    for read in samFile.fetch(until_eof=True):
+        if not read.is_unmapped:
+            alignmentStart=read.reference_start+1
+            readID=read.query_name
+            readIDSplit=readID.split('/')[0]
+            mappedOn=samFile.getrname(read.reference_id)
+            readIDToAttr[readIDSplit]=[mappedOn, alignmentStart]
+        else:
+            readID=read.query_name
+            readIDSplit=readID.split('/')[0]
+            readIDToAttr[readIDSplit]=["*", "0"]            
+            
+    # get the attributes of readIDs 
+    chromToLocations={}   
+    unMappedReadIDs=[]
+    geneIDs=[]     
+    for readID in readIDs:
+        try: 
+            chromosome=readIDToAttr[readID][0]
+            location=readIDToAttr[readID][1]
+            if chromosome!="*":
+                geneID=annotateLocation(chromosome, location, annotation)
+                geneIDs.append(geneID)
+                if chromosome not in chromToLocations:
+                    #print(chromosome, location)
+                    chromToLocations[chromosome]=[location]   
+                    #print(chromToLocations)
+                else:
+                    chromToLocations[chromosome].append(location)                   
+            else:
+                unMappedReadIDs.append(readID)
+        except KeyError:
+            print(readID + " not in alignment file: " )
+    
+    for key in chromToLocations:
+        fivePrime=min(chromToLocations[key])
+        threePrime=max(chromToLocations[key])
+        regionSpan=threePrime-fivePrime
+        numberOfReads=len(chromToLocations[key])
+        print("In region {0}:{1}-{2}, spanning {4}nts there are {3} Reads".format(key, fivePrime, threePrime, numberOfReads, regionSpan))
+    
+    print("Moreover, {0} read(s) were left unmapped:".format(len(unMappedReadIDs)))
+    print(unMappedReadIDs)
+    
+    # Flatten the gene IDs list
+    geneIDsList=[gene for sublist in geneIDs for gene in sublist]
+    countReadsInGenes={gene:geneIDsList.count(gene) for gene in geneIDsList}
+    
+    for gene in countReadsInGenes:
+        print("{1} Reads Mapping On: {0}".format(gene, countReadsInGenes[gene]))
+    
+annotation=getAnnotation(annotationFile)    
+getReadLocation(bamFile, annotation)
---
+++ trackReads.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+# Title: A script to track reads mapped on transcriptome to their location on the genome
+# Author: Ioannis Moustakas
+
+# Read from command line
+# the gene ID I want to trace
+GeneID=$1
+
+# Use the transcriptome file fasta headers info to build a list of tarnscriptIDs
+TranscriptIDs=$(grep $GeneID ../Results/HumanGenome_GRCh38_release83/Homo_sapiens.GRCh38.cdna.all.NoAlternates_FixIDs.fa |cut -d ' ' -f1|cut -d '>' -f 2 )
+
+grep -m 1 $GeneID ../Results/HumanGenome_GRCh38_release83/Homo_sapiens.GRCh38.cdna.all.NoAlternates_FixIDs.fa
+
+TranscriptIDs=( $TranscriptIDs )
+
+echo "Number of Transcripts:" ${#TranscriptIDs[@]}
+
+allReadIDs=$(\
+for TrID in $TranscriptIDs;
+do
+  grep $TrID ../Scratch/mapped_Ioannis/S21_tmap_transc.sam | grep -v "@"|cut -f 1 | sort | uniq 
+done\
+)
+
+
+echo -e "$allReadIDs" | ./readID2GenomicLocation.py ../Scratch/tr_exonmapping_Ioannis/S21_STAR_bowtie.bam  \
+../Results/HumanGenome_GRCh38_release83/Homo_sapiens.GRCh38.83.chr.gtf_GeneOnlySimplified.txt
